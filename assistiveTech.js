0694fba98cb05c0451106b126460dd255e7d3865
George T. Walters II <walterg2@gmail.com>
2011-08-19 14:44:34 -0400

    Code clean up from JS Lint checks

--- assistiveTech.js
+++ assistiveTech.js
@@ -21,179 +21,176 @@
 var assistiveTech = (function () {
 	"use strict";
 	var defaults = {
-		version = 0.1,
-		callBack,
-		cookieName = "assistiveTech",
-		debug = false,
-		divId,
-		flashLocale = "/flash/assistiveTech.swf",
-		flashVersion = "9",
-		success,
-		techAssist = false,
-		writeAnalytics
+		version: 0.1,
+		cookieName: "assistiveTech",
+		debug: false,
+		divId: "assistiveTech",
+		flashLocale: "/flash/assistiveTech.swf",
+		flashVersion: "9",
+		success: null,
+		techAssist: false,
+		writeAnalytics: null
 	},
 		self = this,
 
 //Public Methods
-	getVersion = function () {
-		return version;
-	},
+		getVersion = function () {
+			return self.version;
+		},
 
-	getCookieName = function () {
-		return cookieName;
-	},
+		getCookieName = function () {
+			return self.cookieName;
+		},
 	
-	getCookieValue = function () {
-		var cookieInformation = self.getCookie();
-		return cookieInformation[1];
-	},
+		getCookieValue = function () {
+			var cookieInformation = self.getCookie();
+			return cookieInformation[1];
+		},
 	
-	init = function (options) {
-		if (self.typeOf(options) === 'object') {
-			for (var key in (options || {})) {
-				self.defaults[key] = options[key];
+		init = function (options) {
+			if (self.typeOf(options) === 'object') {
+				for (var key in (options || {})) {
+					self.defaults[key] = options[key];
+				}
 			}
-		}
-		
-		// Test to see if SWFObject is loaded and if not, load it dynamically from Google APIs
-		//ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js
-		
-		if (!self.cookieExists() && !(typeof swfobject.embedSWF === "function") {
-			self.loadSwfObject();
-		} else if (!self.cookieExists()) {
-			self.generateFlash();
-		}
-	},
-	
-	flashSuccess = function (accessibilityFlag) {
-		self.defaults.techAssist = (self.defaults.debug) ? true
-				: accessibilityFlag);
-		self.setCookie();
-		if (self.typeOf(self.defaults.writeAnalytics) === "function") {
-			self.defaults.writeAnalytics((self.defaults.techAssist ? 'Yes' : 'No'));
-		}
-	},
-	
-// Private Methods
-	loadSwfObject = function () {
-		var that = this,
-			body = document.getElementsByTagName("body")[0],
-			script = document.createElement("script");
-		
-		script.type = "text/javascript";
-		script.src = "//ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js";
-		// Mozilla, Safari, Opera, etc
-		script.onload = that.generateFlash;
-		// IE
-		script.onreadystatechange = function () {
-			if (this.readyState === "complete") {
+
+			// Test to see if SWFObject is loaded and if not, load it dynamically from Google APIs
+			//ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js
+
+			if (!self.cookieExists() && !(typeof swfobject.embedSWF === "function") {
+				self.loadSwfObject();
+			} else if (!self.cookieExists()) {
 				self.generateFlash();
 			}
-		};
-		
-		body.appendChild(script);
-	},
+		},
 	
-	generateFlash = function () {
-		var that = this;
-		that.flashVars = {callback:"assistiveTech.flashSuccess"};
-		that.flashParams = {quality:"low",allowScriptAccess:"all"};
-		
-		// Write out the SWFObject
-		swfobject.embedSWF(self.defaults.flashLocale, self.defaults.divID, 1, 1, self.defaults.flashVersion, false, that.flashVars, that.flashParams, false, self.checkFlashInclusion);
-	},
+		flashSuccess = function (accessibilityFlag) {
+			self.defaults.techAssist = (self.defaults.debug) ? true : accessibilityFlag);
+			self.setCookie();
+			if (self.typeOf(self.defaults.writeAnalytics) === "function") {
+				self.defaults.writeAnalytics((self.defaults.techAssist ? 'Yes' : 'No'));
+			}
+		},
 	
-	checkFlashInclusion = function (flag) {
-		if (document.getElementById(self.defaults.divID).type.indexOf("application/x-shockwave-flash") !== -1) {
-			document.getElementById(self.defaults.divID).focus();
-			self.flashSuccess(flag);
-		}
-		else {
+// Private Methods
+		loadSwfObject = function () {
+			var that = this,
+				body = document.getElementsByTagName("body")[0],
+				script = document.createElement("script");
+
+			script.type = "text/javascript";
+			script.src = "//ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js";
+			// Mozilla, Safari, Opera, etc
+			script.onload = that.generateFlash;
+			// IE
+			script.onreadystatechange = function () {
+				if (this.readyState === "complete") {
+					self.generateFlash();
+				}
+			};
+
+			body.appendChild(script);
+		},
+	
+		generateFlash = function () {
+			var that = this;
+			that.flashVars = {callback:"assistiveTech.flashSuccess"};
+			that.flashParams = {quality:"low",allowScriptAccess:"all"};
+
+			// Write out the SWFObject
+			swfobject.embedSWF(self.defaults.flashLocale, self.defaults.divID, 1, 1, self.defaults.flashVersion, false, that.flashVars, that.flashParams, false, self.checkFlashInclusion);
+		},
+	
+		checkFlashInclusion = function (flag) {
+			if (document.getElementById(self.defaults.divID).type.indexOf("application/x-shockwave-flash") !== -1) {
+				document.getElementById(self.defaults.divID).focus();
+				self.flashSuccess(flag);
+			}
+			else {
 			self.flashFailure();
-		}
-	},
-	
-	/**
-	 * Embedding the Flash application failed.
-	 * Display the version message in the assistiveTech div only
-	 * if we've been asked to do so.
-	 */
-	flashFailure = function () {
-		var that = this,
-			flashVersion = swfobject.getFlashPlayerVersion();
-		if (self.defaults.debug === "true") {
-			if (flashVersion && (document.getElementById && (flashVersion.major > 0))) {
-				document.getElementById(self.defaults.replacementDiv).innerHTML = "<p>This sample requires Flash Player version " +
-					self.defaults.flashVersion + ". You have Flash player " +
-					flashVersion.major + "." + flashVersion.minor + "." + flashVersion.rev +
-					" installed. <a href='http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash'>" +
-					" Download the latest Flash Player</a> to run the sample.</p>";
 			}
-		}
-	},
-	
-	typeOf = function (value) {
-		var s = 'null';
-		if (value) {
-			if (typeof value === 'object' && typeof value.length === 'number' &&
-				!(value.propertyIsEnumerable('length')) &&
-				typeof value.splice === 'function') {
-				s = 'array';
-			}
-		}
-		return s;
-	},
-	
-	getCookie = function () {
-		var cookies = document.cookie.split(';'),
-			i = 0,
-			cookieData,
-			cookieName;
-		for ( ; i < cookies.length; i = i + 1) {
-			cookieData = cookieName = null;
-			cookieData = cookies[i].split('=');
-			cookieName = cookieData[0].replace(/^\s+|\s+$/g, '');
-			if (cookieName === self.defaults.cookieName) {
-				return cookieData;
-			}
-		}
-		return false;
-	},
+		},
 
-	/**
-	 *
-	 * return true/false if cookie exists
-	 */
-	cookieExists = function () {
-		var cookieInformation = self.getCookie(),
-			existsFlag = false;
-		if (self.defaults.debug === "true") {
-			existsFlag = false;
-		} else if (cookieInformation && cookieInformation.length > 1) {
-			existsFlag = true;
-		}
-		return existsFlag;
-	},
+		/**
+		 * Embedding the Flash application failed.
+		 * Display the version message in the assistiveTech div only
+		 * if we've been asked to do so.
+		 */
+		flashFailure = function () {
+			var that = this,
+				flashVersion = swfobject.getFlashPlayerVersion();
+			if (self.defaults.debug === "true") {
+				if (flashVersion && (document.getElementById && (flashVersion.major > 0))) {
+					document.getElementById(self.defaults.replacementDiv).innerHTML = "<p>This sample requires Flash Player version " +
+						self.defaults.flashVersion + ". You have Flash player " +
+						flashVersion.major + "." + flashVersion.minor + "." + flashVersion.rev +
+							" installed. <a href='http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash'>" +
+							" Download the latest Flash Player</a> to run the sample.</p>";
+				}
+			}
+		},
 
-	setCookie = function () {
-		var nextMonth = new Date(), expiresDate;
-		nextMonth.setSeconds(0);
-		nextMonth.setMinutes(0);
-		nextMonth.setHours(0);
-		nextMonth.setMonth(nextMonth.getMonth() + 1);
-		nextMonth.setDate(1);
-		expiresDate = new Date(nextMonth);
-		document.cookie = self.defaults.cookieName + "=" +
+		typeOf = function (value) {
+			var s = 'null';
+			if (value) {
+				if (typeof value === 'object' && typeof value.length === 'number' &&
+					!(value.propertyIsEnumerable('length')) &&
+					typeof value.splice === 'function') {
+						s = 'array';
+				}
+			}
+			return s;
+		},
+
+		getCookie = function () {
+			var cookies = document.cookie.split(';'),
+				i = 0,
+				cookieData,
+				cookieName;
+			for ( ; i < cookies.length; i = i + 1) {
+				cookieData = cookieName = null;
+				cookieData = cookies[i].split('=');
+				cookieName = cookieData[0].replace(/^\s+|\s+$/g, '');
+				if (cookieName === self.defaults.cookieName) {
+					return cookieData;
+				}
+			}
+			return false;
+		},
+
+		/**
+		 *
+		 * return true/false if cookie exists
+		 */
+		cookieExists = function () {
+			var cookieInformation = self.getCookie(),
+				existsFlag = false;
+			if (self.defaults.debug === "true") {
+				existsFlag = false;
+			} else if (cookieInformation && cookieInformation.length > 1) {
+				existsFlag = true;
+			}
+			return existsFlag;
+		},
+
+		setCookie = function () {
+			var nextMonth = new Date(), expiresDate;
+			nextMonth.setSeconds(0);
+			nextMonth.setMinutes(0);
+			nextMonth.setHours(0);
+			nextMonth.setMonth(nextMonth.getMonth() + 1);
+			nextMonth.setDate(1);
+			expiresDate = new Date(nextMonth);
+			document.cookie = self.defaults.cookieName + "=" +
 				escape(self.defaults.techAssist) + "; expires=" +
 				expiresDate.toGMTString() + "; path=/";
-	};
+		};
 
-	return {
-		init: init,
-		flashSuccess: flashSuccess,
-		getCookieName: getCookieName,
-		getCookieValue: getCookieValue,
-		getVersion: getVersion;
-	}
-	
+		return {
+			init: init,
+			flashSuccess: flashSuccess,
+			getCookieName: getCookieName,
+			getCookieValue: getCookieValue,
+			getVersion: getVersion;
+		};
 }());
